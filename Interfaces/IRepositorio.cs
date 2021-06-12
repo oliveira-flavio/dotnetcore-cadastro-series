696e8fb5fd2bc3a76f4e7c2494ec6a97dfd1bce9
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-12 14:55:52 -0300

    Adicionado Interface IRepositório

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries.Interfaces
+{
+    interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int Id);
+        void Insere(T entidade);
+        void Exclui(int Id);
+        void Atualiza(int Id, T entidade);
+        int ProximoId();
+    }
+}
