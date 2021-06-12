cf6829f41f8837d6235f25e77f314da905663483
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-12 15:06:27 -0300

    Implementação da Interface IRepositorio na classe SerireRepositorio

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using CadastroSeries.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries.Classes
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int Id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
