c1737034b6a5ed16d77f48ca2cba7e565630d7df
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-06-12 14:39:03 -0300

    Commit inicial

---
+++ Classes/Serie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeries.Classes
+{
+    class Serie : EntidadeBase
+    {
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + Genero + Environment.NewLine;
+            retorno += "Titulo: " + Titulo + Environment.NewLine;
+            retorno += "Descrição: " + Descricao + Environment.NewLine;
+            retorno += "Ano de início: " + Ano + Environment.NewLine;
+
+            return retorno;
+        }
+    }
+}
